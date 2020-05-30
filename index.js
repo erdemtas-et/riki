f4ad7e787093534fb960adfe816c9c8a5406f779
erdemtas-et <60156255+erdemtas-et@users.noreply.github.com>
2020-05-30 11:32:29 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+$(function()
+ {
+    $("h1").on({
+        mouseover:function(){
+            
+             $(this).css("color","orange");
+        },
+        
+        mouseleave:function(){
+            $(this).css("color","black");
+        }
+        
+        
+        
+        
+    });
+    
+    
+    
+});
